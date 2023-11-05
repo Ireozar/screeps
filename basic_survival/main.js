d1355efd5ade90aabc493ae6ed18d22c9328bea5
Ireozar <109338281+Ireozar@users.noreply.github.com>
2023-11-05 10:16:15 +0100

    Update main.js

--- basic_survival/main.js
+++ basic_survival/main.js
@@ -1,4 +1,7 @@
-var harvester = require("role.harvester");
+var roleHarvester = require("role.harvester");
 module.exports.loop = function() {
-
+  for (var i in Game.screeps) {
+    var screep = Game.screeps[i];
+    roleHarvester.run(screep);
+  }
 }
