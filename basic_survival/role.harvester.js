cdf48b231e3eb9160a556f21394f5a98cdca94fe
Ireozar <109338281+Ireozar@users.noreply.github.com>
2023-11-06 11:42:14 +0100

    Update role.harvester.js

--- basic_survival/role.harvester.js
+++ basic_survival/role.harvester.js
@@ -7,7 +7,7 @@ module.exports = {
         }
     }
     else {
-        if (creep.store(Game.spawns["Spawn1"],RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        if (creep.transfer(Game.spawns["Spawn1"],RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(Game.spawns["Spawn1"]);
         }
     }
