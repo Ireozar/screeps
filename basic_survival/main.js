ba81f3a2ab5a1541b9b64375eec1b308a7fbf4e4
Ireozar <109338281+Ireozar@users.noreply.github.com>
2023-11-07 11:35:49 +0100

    Update main.js

--- basic_survival/main.js
+++ basic_survival/main.js
@@ -1,5 +1,8 @@
 var roleHarvester = require("role.harvester");
 module.exports.loop = function() {
+  if (Game.screeps.length < 1) {
+    Game.spawns["Spawn1"].spawnCreep([WORK,CARRY,MOVE],"Harvester1",{memory:{role:"harvester"}});
+  }
   for (var i in Game.creeps) {
     var creep = Game.creeps[i];
     switch (creep.memory.role) {
