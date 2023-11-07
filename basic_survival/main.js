2821ea6dbd23dcd61e5eb4c80fbd730aa7943075
Ireozar <109338281+Ireozar@users.noreply.github.com>
2023-11-07 11:44:05 +0100

    Update main.js

--- basic_survival/main.js
+++ basic_survival/main.js
@@ -4,9 +4,9 @@ module.exports.loop = function() {
     Game.spawns["Spawn1"].spawnCreep([WORK,CARRY,MOVE],"Harvester",{memory:{role:"harvester"}});
   }
   for (var i in Game.creeps) {
-    if(!Game.creeps[name]) {
-        delete Memory.creeps[name];
-	    console.log('Clearing non-existing creep memory:', name);
+    if(!Game.creeps[i]) {
+        delete Memory.creeps[i];
+	    console.log('Clearing non-existing creep memory:', i);
     }
     var creep = Game.creeps[i];
     switch (creep.memory.role) {
